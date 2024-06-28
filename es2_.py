28558caf98efea9f4400b7a6d38c468ea34b404a
wellarzin <brunodiaswwll@gmail.com>
2024-06-28 17:54:26 -0300

    fix: fixing errors

--- es2_.py
+++ es2_.py
@@ -1,4 +1,3 @@
-# Bruno: Criando a classe de Quiz de Esportes com um construtor com as perguntas, opções, respostas corretas e o score.
 class QuizDeEsportes:
     def __init__(self):
         self.perguntas = [
@@ -20,16 +19,15 @@ class QuizDeEsportes:
         ]
         self.score = 0
 
-    def fazerpergunta(pergunta, opcoes, respostacorreta):
+    def fazer_pergunta(self, pergunta, opcoes, resposta_correta):
         print(pergunta)
         for i, opcao in enumerate(opcoes, 1):
             print(f"{i}. {opcao}")
         resposta = input("Digite o número da sua resposta: ")
 
-        # * Murilo: Refatorando o código para tratar exceções e evitar erros de execução.
         try:
             resposta = int(resposta)
-            if opcoes[resposta - 1] == respostacorreta:
+            if opcoes[resposta - 1] == resposta_correta:
                 print("Resposta correta!")
                 return True
             else:
@@ -39,7 +37,6 @@ class QuizDeEsportes:
             print("Entrada inválida! Por favor, digite um número válido.")
             return False
     
-    #Lucas: Cria um método para iniciar o quiz e percorrer todas as perguntas, chamando o método fazer_pergunta. 
     def iniciar_quiz(self):
         for pergunta in self.perguntas:
             if self.fazer_pergunta(pergunta["pergunta"], pergunta["opcoes"], pergunta["resposta_correta"]):
@@ -48,5 +45,5 @@ class QuizDeEsportes:
         print(f"Você acertou {self.score} de {len(self.perguntas)} perguntas.")
 
 if __name__ == "__main__":
-    quiz = QuizDeEsportes() #Bruno: Cria um objeto da classe QuizDeEsportes.
+    quiz = QuizDeEsportes()
     quiz.iniciar_quiz()
